378c723a09657bf47742458316a1ca267a17855b
Alain Frisch <alain@frisch.fr>
2007-03-22 17:24:30 +0000

    *** empty log message ***

--- reloc.ml
+++ reloc.ml
@@ -1,6 +1,56 @@
+(* Internal representation of COFF object files *)
+
+type symbol = {
+  mutable sym_pos: int;
+  sym_name: string;
+  mutable value: int32;
+  mutable section: [ `Num of int | `Section of section ];
+  stype: int;
+  storage: int;
+  auxn: int;
+  auxs: string;
+}
+
+and reloc = {
+  addr: int32;
+  symbol: symbol;
+  rtype: int;
+}
+
+and section = {
+  mutable sec_pos: int;
+  sec_name: string;
+  vsize: int32;
+  mutable data: string;
+  mutable relocs: reloc list;
+  sec_opts: int32;
+}
+
+type coff = {
+  machine: int;
+  date: int32;
+  mutable sections: section list;
+  mutable symbols: symbol list;
+  opts: int;
+}
+
+
+(* Misc *)
+
+
 let (++) = Int32.add
 let (&&&) = Int32.logand
 let (>>>) = Int32.shift_right_logical
+
+let filter f l =
+  let rec aux accu = function
+    | [] -> accu
+    | hd::tl -> if f hd then aux (hd::accu) tl else aux accu tl
+  in
+  aux [] l
+
+
+(* Tools to read/write binary data *)
 
 let mk_int32 a b c d =
   let a = Int32.of_int a 
@@ -53,6 +103,16 @@ let strz buf loc ?(max=String.length buf - loc) c =
 let emit_zero oc n =
   for i = 1 to n do output_char oc '\000' done
 
+let delayed_ptr oc f =
+  let bak = pos_out oc in
+  emit_int32 oc 0l;
+  (fun () ->
+     patch_int32 oc bak (Int32.of_int (pos_out oc));
+     f ()
+  )
+
+(* Human readable pretty-printers *)
+
 let flags x =
   let b = Buffer.create 16 in
   for i = 0 to 31 do
@@ -78,83 +138,17 @@ let rec dump ic pos len w =
   flush stdout;
   dump ic (pos + l) (len - l) w
 
-let delayed_ptr oc f =
-  let bak = pos_out oc in
-  emit_int32 oc 0l;
-  (fun () ->
-     patch_int32 oc bak (Int32.of_int (pos_out oc));
-     f ()
-  )
-
-let filter f l =
-  let rec aux accu = function
-    | [] -> accu
-    | hd::tl -> if f hd then aux (hd::accu) tl else aux accu tl
-  in
-  aux [] l
-
-type symbol = {
-  mutable sym_pos: int;
-  sym_name: string;
-  mutable value: int32;
-  mutable section: [ `Num of int | `Section of section ];
-  stype: int;
-  storage: int;
-  auxn: int;
-  auxs: string;
-}
-
-and reloc = {
-  addr: int32;
-  symbol: symbol;
-  rtype: int;
-}
-
-and section = {
-  mutable sec_pos: int;
-  sec_name: string;
-  vsize: int32;
-  mutable data: string;
-  mutable relocs: reloc list;
-  sec_opts: int32;
-}
-
-type coff = {
-  machine: int;
-  date: int32;
-  mutable sections: section list;
-  mutable symbols: symbol list;
-  opts: int;
-}
-
 module Symbol = struct
   let counter = ref 0
-
-  let gen_sym () =
-    incr counter;
-    Printf.sprintf "DREL$%i" !counter
+  let gen_sym () = incr counter; Printf.sprintf "DREL$%i" !counter
 
   let intern sec addr =
-    { sym_pos = (-1);
-      sym_name = gen_sym();
-      value = addr;
-      section = `Section sec;
-      storage = 3;
-      stype = 0;
-      auxn = 0;
-      auxs = "";
-    }
+    { sym_pos = (-1); sym_name = gen_sym(); value = addr; 
+      section = `Section sec; storage = 3; stype = 0; auxn = 0; auxs = ""; }
 
   let export name sec addr = 
-    { sym_pos = (-1);
-      sym_name = name;
-      value = addr;
-      section = `Section sec;
-      storage = 2;
-      stype = 0;
-      auxn = 0;
-      auxs = "";
-    }
+    { sym_pos = (-1); sym_name = name; value = addr;
+      section = `Section sec; storage = 2; stype = 0; auxn = 0; auxs = ""; }
 
 
   let get strtbl ic pos =
@@ -173,12 +167,20 @@ module Symbol = struct
     }
 
   let is_extern = function
-    | { storage = 2; section = `Num 0; value = 0l } -> true
+    |  { storage = 2; section = `Num 0; value = 0l } -> true
     | _ -> false
 
   let is_export = function
     | { storage = 2; section = `Section _ } -> true
+    | { storage = 2; section = `Num 0; value = 0l } -> false
+    | { storage = 2; section = `Num 0 } -> true
     | _ -> false
+
+  let is_defin = function
+    | { storage = 2; section = `Section _ } -> true
+    | { storage = 2; section = `Num 0; value = 0l } -> false
+    | { storage = 2; section = `Num (0 | 0xffff) } -> true
+    | _ -> false 
 
   let dump s = 
     Printf.printf " %s: " s.sym_name;
@@ -205,6 +207,8 @@ module Symbol = struct
 	  Printf.printf "static %s @ 0x%08lx\n" sect s.value
       | { storage = 103 } ->
 	  Printf.printf "filename %s\n" (strz s.auxs 0 '\000')
+      | { storage = 105 } ->
+	  Printf.printf "weak ext\n"
       | _ ->
 	  Printf.printf 
 	    "value=0x%08lx, sect=%s, storage=%s, aux=%i\n"
@@ -216,15 +220,13 @@ module Symbol = struct
 	  emit_zero oc (8 - String.length s.sym_name))
     else (emit_zero oc 4; emit_int32 oc (strtbl s.sym_name));
     emit_int32 oc s.value;
-    emit_int16 oc (match s.section with
-		     | `Num i -> i
-		     | `Section sec -> 
-			 if sec.sec_pos <= 0 then (
-			   Printf.eprintf "Cannot emit section for symbol %s"
-			     s.sym_name;
-			   exit 1
-			 );
-			   sec.sec_pos);
+    let sec = match s.section with
+      | `Num i -> i
+      | `Section sec when sec.sec_pos <= 0 ->
+	  Printf.eprintf "Cannot emit section for symbol %s" s.sym_name;
+	  exit 1
+      | `Section sec -> sec.sec_pos in
+    emit_int16 oc sec;
     emit_int16 oc s.stype;
     emit_int8 oc s.storage;
     emit_int8 oc s.auxn;
@@ -245,10 +247,8 @@ module Reloc = struct
     }
 
   let dump x =
-    Printf.printf " Reloc %ld -> %s, type 0x%04x\n"
-      x.addr
-      x.symbol.sym_name
-      x.rtype
+    Printf.printf " Reloc %ld -> %s, type 0x%04x\n" 
+      x.addr x.symbol.sym_name x.rtype
 
   let put oc x =
     emit_int32 oc x.addr;
@@ -267,7 +267,7 @@ module Section = struct
     vsize = 0l;  sec_opts = flags;
   }
 
-  let get strtbl symtbl ic base =
+  let get filebase strtbl symtbl ic base =
     let buf = read ic base 40 in
     let size = int32_ buf 16 in
     let name = 
@@ -284,19 +284,21 @@ module Section = struct
 
 
     let relocs =
-      let base = int32_ buf 24 in
+      let base = filebase + int32_ buf 24 in
       let r = ref [] in
       for i = 0 to (int16 buf 32) - 1 do
 	r := Reloc.get symtbl ic (base + 10 * i) :: !r
-	  (* file base ? *)
       done;
       !r
     in
 
+    let data = read ic (filebase + int32_ buf 20) size in
+(*    if name = ".drectve" then dump ic (filebase + int32_ buf 20) size 16;  *)
+
     { sec_pos = (-1);
       sec_name = name;
       vsize = int32 buf 8;
-      data = read ic (int32_ buf 20) size; (* todo: file base *)
+      data = data;
       relocs = relocs;
       sec_opts = int32 buf 36
     }
@@ -368,7 +370,7 @@ module Coff = struct
     let sectable = base + 20 + opthdr in
     let sections =
       Array.init (int16 buf 2)
-	(fun i -> Section.get strtbl symtbl ic (sectable + 40 * i))
+	(fun i -> Section.get base strtbl symtbl ic (sectable + 40 * i))
     in
     List.iter
       (fun s ->
@@ -398,11 +400,8 @@ module Coff = struct
     let () =
       let no = ref 0 in
       List.iter
-	(fun s ->
-	   incr no;
-	   assert(s.sec_pos < 0);
-	   s.sec_pos <- !no
-	) x.sections
+	(fun s -> incr no; assert(s.sec_pos < 0); s.sec_pos <- !no)
+	x.sections
     in
 
     emit_int16 oc (List.length x.sections);
@@ -454,32 +453,34 @@ end
 module Import = struct
   let read ic pos size =
     let buf = read ic pos size in
-    Printf.printf "Import header. Version = %i\n" (int16 buf 4);
+    let w = int16 buf 18 in
+    let name = strz buf 20 '\000' in
+(*    Printf.printf "Import header. Version = %i\n" (int16 buf 4);
     Printf.printf " machine     = 0x%x\n" (int16 buf 6);
     Printf.printf " time stamp  = 0x%lx\n" (int32 buf 8);    
     Printf.printf " size data   = %ld\n" (int32 buf 12);
     Printf.printf " ord/hint    = %i\n" (int16 buf 16);
-    let w = int16 buf 18 in
     Printf.printf " type        = %i\n" (w land 0b11);
-    Printf.printf " name type   = %i\n" ((w land 0b11100) lsr 2);
-    let name = strz buf 20 '\000' in
+    Printf.printf " name type   = %i\n" ((w land 0b11100) lsr 2); 
     Printf.printf " symbol      = %s\n" name;
     Printf.printf " DLL         = %s\n" 
-      (strz buf (21 + String.length name) '\000')
+      (strz buf (21 + String.length name) '\000'); *)
+    name, w
+
 end
 
 module Lib = struct
   let magic_lib = "!<arch>\n"
 
-  let read_lib ic  =
-    (* note: member names currently unused *)
+  let read_lib ic =
     let strtbl = ref "" in
+    let imports = ref [] and objects = ref [] in
     let obj size name = 
-      Printf.printf "-> %s (size %i)\n" name size; 
+(*      Printf.printf "-> %s (size %i)\n" name size;  *)
       let pos = pos_in ic in
       if (size > 18) && (read ic pos 4 = "\000\000\255\255") 
-      then Import.read ic pos size
-      else (*read_obj ic pos*) ()
+      then imports := Import.read ic pos size :: !imports
+      else objects := (name, Coff.get ic pos) :: !objects
     in
     let rec read_member () =
       let buf = read ic (pos_in ic) 60 in
@@ -498,16 +499,19 @@ module Lib = struct
       seek_in ic (base + size + size mod 2); 
       read_member ()
     in
-    (try read_member () with End_of_file -> ())
+    (try read_member () with End_of_file -> ());
+    !objects,!imports
 
   let read filename =
     let ic = open_in_bin filename in
     try
-      if in_channel_length ic > String.length magic_lib
-	&& read ic 0 (String.length magic_lib) = magic_lib 
-      then read_lib ic
-      else print_string "Not a lib\n";
-      close_in ic
+      let r = 
+	if in_channel_length ic > String.length magic_lib
+	  && read ic 0 (String.length magic_lib) = magic_lib 
+	then `Lib (read_lib ic)
+	else `Obj (Coff.get ic 0) in
+      close_in ic;
+      r
     with exn ->
       close_in ic;
       raise exn
@@ -538,10 +542,6 @@ let int32_to_buf b i =
   Buffer.add_char b (Char.chr ((i lsr 16) land 0xff));
   Buffer.add_char b (Char.chr ((i lsr 24) land 0xff))
 
-let str_to_buf b s =
-  Buffer.add_string b s;
-  for i = 1 to 4 - ((String.length s) mod 4) do Buffer.add_char b '\000' done
-
 let make_reloctable x p =
   let sect = Section.create ".dynrel" 0xc0300040l in
   let data = Buffer.create 1024 in
@@ -549,7 +549,7 @@ let make_reloctable x p =
   let strsym = Symbol.intern sect 0l in
 
   (* TODO: share symbol names *)
-  (* TODO: use a single table per section *)
+  (* TODO: use a single symbol per section *)
   let syms = ref [] in
   let reloc sec rel =
     if p rel.symbol then (
@@ -628,7 +628,122 @@ let parse_obj s =
   else
     Coff.dump coff
 
+module StrSet = Set.Make(String)
+
+let parse_libpath s =
+  let n = String.length s in
+  let rec aux l = 
+    if l >= n then []
+    else 
+      try
+	let i = String.index_from s l ';' in
+	String.sub s l (i - l) :: aux (succ i)
+      with Not_found -> [ String.sub s l (n - l) ]
+  in
+  aux 0
+
+(* TODO: write a real lexer for directives (allow the -opt syntax) *)
+let parse_directives s =
+  let deflibs = ref [] in
+  let rec aux l =
+    let i = String.index_from s l '/' in
+    if String.uppercase (String.sub s (i+1) 11) = "DEFAULTLIB:" then (
+      let j = String.index_from s (i+12) ' ' in
+      let lib = String.sub s (i+12) (j-i-12) in
+      let lib = 
+	if lib.[0] = '"' && lib.[String.length lib - 1]  = '"'
+	then String.sub lib 1 (String.length lib - 2)
+	else lib in
+      deflibs := lib :: !deflibs;
+      aux j
+    ) else
+      aux (succ i)
+  in
+  (try aux 0 with _ -> ());
+  !deflibs
+      
+
 let () =
-  parse_obj Sys.argv.(1);  
-(*  Lib.read Sys.argv.(1) *)
-  ()
+(*  parse_obj Sys.argv.(1);   *)
+
+  let libpath = parse_libpath (try Sys.getenv "LIB" with Not_found -> "") in
+  let defined = ref StrSet.empty in
+  let files = ref [] in
+  let loaded = ref StrSet.empty in
+
+  let find_file fn =
+    if Sys.file_exists fn then fn
+    else 
+      Filename.concat
+	(List.find (fun s -> Sys.file_exists (Filename.concat s fn)) libpath)
+	fn in
+
+  let find_file fn =
+    try find_file fn
+    with Not_found ->
+      try find_file (fn ^ ".lib")
+      with Not_found -> failwith ("Cannot find file " ^ fn) in
+
+  let needed accu obj =
+    List.fold_left
+      (fun accu sym -> 
+	 if Symbol.is_extern sym
+	 then StrSet.add sym.sym_name accu
+	 else accu
+      )
+      accu
+      obj.symbols
+  in
+
+  let rec collect_exports_obj obj =
+    (try
+       let dirsec = 
+	 List.find (fun s -> s.sec_name = ".drectve") obj.sections in
+       let deflibs = parse_directives dirsec.data in
+(*       List.iter (fun s -> Printf.printf "DEFLIB: %s\n" s) deflibs; *)
+       List.iter (add_file true) deflibs
+     with Not_found -> ());
+
+    List.iter
+      (fun sym -> 
+	 if Symbol.is_defin sym 
+	 then defined := StrSet.add sym.sym_name !defined
+      )
+      obj.symbols 
+
+  and collect_exports = function
+    | `Obj obj -> collect_exports_obj obj
+    | `Lib (objs,imports) ->
+	List.iter (fun (_,obj) -> collect_exports_obj obj) objs;
+	List.iter
+	  (fun (s,_) -> 
+	     defined := StrSet.add s (StrSet.add ("__imp_" ^ s) !defined))
+	  imports
+
+  and add_file default fn =
+    let fn = find_file fn in
+    if StrSet.mem fn !loaded then ()
+    else (loaded := StrSet.add fn !loaded;
+	  let x = Lib.read fn in
+	  files := (default, fn, x) :: !files;
+	  collect_exports x)
+  in
+
+  for i = 1 to Array.length Sys.argv - 1 do add_file false Sys.argv.(i) done;
+  let files = !files in
+
+  List.iter 
+    (fun (default,fn,x) ->
+       if not default then begin
+	 let needed = match x with
+	   | `Obj obj -> needed StrSet.empty obj
+	   | `Lib (objs,_) -> 
+	     List.fold_left (fun accu (_,obj) -> needed accu obj)
+	       StrSet.empty objs in
+	 let relocs = StrSet.diff needed !defined in
+	 StrSet.iter
+	   (fun s -> Printf.printf "%s: %s\n" fn s)
+	   relocs
+       end
+    )
+    files
