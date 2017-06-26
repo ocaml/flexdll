99394652fbfc9c7649256771ac1123cecdcfcc94
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-06-26 13:33:57 +0200

    Add wide-character version of flexdll_dlopen

--- flexdll.h
+++ flexdll.h
@@ -12,6 +12,8 @@
 #ifndef FLEXDLL_H
 #define FLEXDLL_H
 
+#include <wchar.h>
+
 #define FLEXDLL_RTLD_GLOBAL 0x0001
 #define FLEXDLL_RTLD_LOCAL  0x0000
 #define FLEXDLL_RTLD_NOEXEC 0x0002
@@ -22,6 +24,10 @@ extern "C"
 #endif
 
 void *flexdll_dlopen(const char *, int);
+#ifndef CYGWIN
+void *flexdll_wdlopen(const wchar_t *, int);
+#endif
+
 void *flexdll_dlsym(void *, const char *);
 void flexdll_dlclose(void *);
 char *flexdll_dlerror(void);
